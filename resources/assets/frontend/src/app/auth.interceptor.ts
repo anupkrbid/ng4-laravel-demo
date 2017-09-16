d3f638511807d57f614a1ca4f6a8cd056508199a
anupkrbid <anupbid94@gmail.com>
2017-09-17 03:18:59 +0530

    cose to handel token in interceptor added

--- resources/assets/frontend/src/app/auth.interceptor.ts
+++ resources/assets/frontend/src/app/auth.interceptor.ts
@@ -1,10 +1,38 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+// import { Router } from '@angular/router';
+import 'rxjs/add/operator/do';
+
+// import { AuthService } from './auth.service';
 
 export class AuthInterceptor implements HttpInterceptor {
 
+	// constructor( private router: Router,
+	//              private authService: AuthService ) { }
+
 	intercept( req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
-		console.log('intersepted');
-		return next.handle(req);
+		return next.handle( req )
+			.do(
+				( event: HttpEvent<any> ) => {
+					if ( event instanceof HttpResponse ) {
+						// do stuff with response if you want
+					}
+				},
+				( err: any ) => {
+					if ( err instanceof HttpErrorResponse ) {
+							console.log(err.status);
+
+						// if ( err.status === 401 ) {
+						//
+						// 	localStorage.removeItem( 'token' );
+						// 	this.authService.loggedIn.next();
+						// 	this.router.navigate( [ '/sign-in' ] );
+						//
+						// }
+
+					}
+				}
+			);
 	}
+
 }
