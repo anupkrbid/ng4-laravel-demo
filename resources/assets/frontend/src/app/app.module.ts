e822d588e09eea55679e52a7d10118401712ad92
anupkrbid <anupbid94@gmail.com>
2017-09-16 23:57:50 +0530

    Interseptor Implemented

--- resources/assets/frontend/src/app/app.module.ts
+++ resources/assets/frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth.guard';
+import { AuthInterceptor } from './auth.interceptor';
+import { AuthService } from './auth.service';
 import { HeaderComponent } from './header/header.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -13,8 +15,6 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteComponent } from './quote/quote.component';
 import { NewQuoteComponent } from './new-quote/new-quote.component';
 import { QuoteService } from './quote.service';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
 import { NotAuthGuard } from './not-auth.guard';
 
 @NgModule( {
@@ -29,7 +29,6 @@ import { NotAuthGuard } from './not-auth.guard';
 	],
 	imports : [
 		BrowserModule,
-		HttpModule,
 		HttpClientModule,
 		FormsModule,
 		ReactiveFormsModule,
@@ -39,7 +38,8 @@ import { NotAuthGuard } from './not-auth.guard';
 		QuoteService,
 		AuthService,
 		AuthGuard,
-		NotAuthGuard
+		NotAuthGuard,
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 	],
 	bootstrap : [ AppComponent ]
 } )
